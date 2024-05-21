696b6e4a15cfbdd4d751484bdd70e85c78769a1b
MauFGarzo <mauricioflores123321@gmail.com>
2024-05-21 17:14:59 -0400

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Activities
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
